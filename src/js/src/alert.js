2167b88dca3224a7e0e34a9d446d44f51052a3bd
mkfizi <mkfizi93@gmail.com>
2022-12-20 00:07:37 +0800

    @ alert: +initialize

---
+++ src/js/src/alert.js
@@ -0,0 +1,50 @@
+/**
+ * --------------------------------------------------------------------------
+ * Base Component JS (v0.2.0): alert.js
+ * Licensed under MIT (https://github.com/mkfizi/base-component-js/blob/main/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+
+"use strict";
+
+import Component from "./component.js";
+
+class Alert extends Component {
+    /**
+     * Alert constructor.
+     * @param {element} element 
+     */
+    constructor(element) {
+        super(element);
+
+       // -------------------------< Put additional codes here >-------------------------
+       // Element's parent element.
+       this.parentElement = this.element.parentElement;
+    }
+
+    /**
+     * Alert event handler.
+     * @param {object} event 
+     */
+    handleEvent(event) {
+        super.handleEvent(event);
+        
+        // If [data-bc-toggle] attribute is not defined on element with [aria-controls] attribute.
+        if (!event.target.hasAttribute("data-bc-toggle")) this.hide();
+
+        // -------------------------< Put additional codes here >-------------------------
+    }
+
+    /**
+     * Alert hide handler.
+     */
+    hide() {
+        super.hide();
+
+        this.element.remove();
+
+        // -------------------------< Put additional codes here >-------------------------
+    }
+}
+
+export default Alert;
